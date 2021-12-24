e11d285ca50b3d386587b18d53255c9207f060b3
Intern-Yago <yagovictorbotafogo@gmail.com>
2021-12-24 13:51:02 -0300

    add titulo e subtitulo

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -11,7 +11,18 @@ export function SignIn() {
 
   return (
     <View style={styles.container}>
-      <Image source={IlustrationImg}/>
+      <Image source={IlustrationImg} style={styles.image}/>
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+      </View>
       
       <StatusBar style="auto" />
     </View>
