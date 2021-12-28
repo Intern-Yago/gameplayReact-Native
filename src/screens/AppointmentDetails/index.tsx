c32a28cfaa58be29129d06a8e30e5fb0eea16ed6
Intern-Yago <yagovictorbotafogo@gmail.com>
2021-12-27 21:18:32 -0300

    add members

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,41 +1,78 @@
-import React from "react";
-import {Fontisto} from '@expo/vector-icons'
-import { ImageBackground, Text, View } from "react-native";
-import { BorderlessButton } from "react-native-gesture-handler";
+import React from 'react';
+import { Fontisto } from '@expo/vector-icons';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
-import { Background } from "../../components/Background";
-import { theme } from "../../global/styles/theme";
-import { Header } from "../../components/Header";
-import BannerImg from '../../assets/banner.png'
-import { styles } from "./style";
+import {
+  ImageBackground,
+  Text,
+  View,
+  FlatList
+} from 'react-native';
+
+import { theme } from '../../global/styles/theme';
+import { styles } from './style';
+import BannerImg from '../../assets/banner.png';
+
+import { ListDivider } from '../../components/ListDivider';
+import { Background } from '../../components/Background';
+import { ListHeader } from '../../components/ListHeader';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { Member } from '../../components/Member';
+import { Header } from '../../components/Header';
 
 export function AppointmentDetails(){
+    const members = [
+        {
+          id: '1',
+          username: 'Syri',
+          avatar_url: 'https://github.com/Intern-Yago.png',
+          status: 'online'
+        },
+        {
+          id: '2',
+          username: 'Syri',
+          avatar_url: 'https://github.com/Intern-Yago.png',
+          status: 'offline'
+        }
+      ]
     return(
         <Background>
-            <Header
-            title="Detalhes"
-            action={
+            <Header 
+                title="Detalhes"
+                action={
                 <BorderlessButton>
-                    <Fontisto
-                        name="share"
-                        size={24}
-                        color={theme.color.primary}
+                    <Fontisto 
+                    name="share"
+                    size={24}
+                    color={theme.color.primary}
                     />
                 </BorderlessButton>
-            }
+                }
             />
             <ImageBackground 
-            source={BannerImg}
-            style={styles.banner}> 
+                source={BannerImg}
+                style={styles.banner}
+            >
                 <View style={styles.bannerContent}>
-                    <Text style={styles.title}>
-                        Lendários
+                    <Text style={styles.title}> 
+                        Lendários 
                     </Text>
                     <Text style={styles.subtitle}>
-                        Hora da guerra
+                        É hoje que vamos chegar ao challenger sem perder uma partida da md10
                     </Text>
                 </View> 
             </ImageBackground>
+            <ListHeader 
+                title="Jogadores"
+                subtitle="Total 3"
+            />
+            <FlatList
+                data={members}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => (
+                    <Member data={item}/>
+                )}
+            />
         </Background>
     )
 }
