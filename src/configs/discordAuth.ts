3c578e804b834f3a33224a0e6bc3168811aa9a86
Intern-Yago <yagovictorbotafogo@gmail.com>
2026-08-03 08:03:10 -0300

    fix(eas): link Expo owner and slug in app.json and use --no-wait with status polling for builds

--- src/configs/discordAuth.ts
+++ src/configs/discordAuth.ts
@@ -1,10 +1,10 @@
-const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2F%40anonymous%2Fgameplay-8409f062-ce02-455d-87f8-65139e9df23e'
+const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2F%40syri_cotocs%2Fgameplayreact-native';
 const SCOPE = 'identify%20connections%20email%20guilds';
 const RESPONSE_TYPE = 'token';
-const CLIENT_ID = '925579035755577374'
+const CLIENT_ID = '925579035755577374';
 const CDN_IMAGE = 'https://cdn.discordapp.com';
 
-export{
+export {
     CDN_IMAGE,
     SCOPE,
     RESPONSE_TYPE,
