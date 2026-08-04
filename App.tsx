931b5e8a427b3f3d169ee7b323dfde13818d15aa
Intern-Yago <yagovictorbotafogo@gmail.com>
2026-08-03 22:50:28 -0300

    chore: upgrade to Expo SDK 51, fix Gradle Boost download error and achieve 100% expo-doctor pass

--- App.tsx
+++ App.tsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import {useFonts} from 'expo-font'
-import { StatusBar,LogBox } from 'react-native'
-import AppLoading from 'expo-app-loading'
-import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
-import {Rajdhani_500Medium, Rajdhani_700Bold} from "@expo-google-fonts/rajdhani"
+import React from 'react';
+import { useFonts } from 'expo-font';
+import { StatusBar, LogBox } from 'react-native';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani";
 
 LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.']);
 
-import { Routes } from './src/routes'
-import { Background } from './src/components/Background'
-
-import { AuthProvider } from './src/hooks/auth'
+import { Routes } from './src/routes';
+import { Background } from './src/components/Background';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
-  const [fonstLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium,
     Rajdhani_500Medium,
     Rajdhani_700Bold
-  })
-  if(!fonstLoaded){
-    return <AppLoading/>
+  });
+
+  if (!fontsLoaded) {
+    return null;
   }
+
   return (
     <Background>
-      <StatusBar 
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <AuthProvider>
-          <Routes/>
-        </AuthProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </Background>
   );
 }
